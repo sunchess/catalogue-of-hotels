c0d61b91606bde5aeea70ecb2a76dd46bc49bb05
sunchess <sun@sun-laptop.(none)>
2010-10-21 18:26:41 +0400

    added styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if user_signed_in?
+     flash[:error] = t("access_denied")
+      redirect_to root_url
+    else
+      authenticate_user!
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
   
   def flashes
     return nil if flash.empty?
-    
     html = String.new
-    %w{notice error}.each do | f |
+    %w{notice error alert}.each do | f |
       if flash[f.to_sym]
          html << content_tag(:div,  flash[f.to_sym], :class=>f) 
       end
