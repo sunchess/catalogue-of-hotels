668b08923b3fe0992399d83004f68963d334570c
sunchess <sun@sun-laptop.(none)>
2011-01-19 22:53:23 +0300

    little changes

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -10,7 +10,7 @@ module PlacesHelper
   def children_places_for place
     if place.children.any?
       capture_haml do
-        haml_tag :div, :class=>"item" do
+        haml_tag :div, :class=>"item", :style=>"margin-top: 5px;" do
           haml_tag :div, :id=>"place_children" do
             haml_tag :div, t("places.children.title"), :class=>"title"
             place.children.each do |child|
