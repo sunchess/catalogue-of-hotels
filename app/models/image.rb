e7c99c1990edf18587d914af667b785510ddd498
sunchess <sun@sun-laptop.(none)>
2011-01-27 23:14:39 +0300

    change image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -33,6 +33,6 @@ class Image < ActiveRecord::Base
     :large =>   "500x500>" }
 
   validates_attachment_presence :image
-  validates_attachment_size :image, :less_than => 3.megabytes
+  validates_attachment_size :image, :less_than => 5.megabytes
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 end
